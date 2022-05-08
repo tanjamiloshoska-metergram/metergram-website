e2a8412f6d20e181e15edbfb3a508989529cb5ec
tanjamiloshoska-metergram <tanja.miloshoska@metergram.com>
2022-05-08 20:15:58 +0200

    fixing styling

--- src/containers/ServicesPage/ServicesPage.jsx
+++ src/containers/ServicesPage/ServicesPage.jsx
@@ -1,15 +1,12 @@
 import styles from "./ServicesPage.module.css";
-import triangle from "../../assets/triangle.png";
 const ServicesPage = () => {
   return (
     <div className={styles.servicesPageWrapper}>
       <a className={styles.menuLink} href="/services/dedicated-team">
-        <img className={styles.triangleImg} src={triangle} alt="option" />
         Dedicated Team
       </a>
 
       <a className={styles.menuLink} href="services/custom-software">
-        <img className={styles.triangleImg} src={triangle} alt="option" />
         Custom Software
       </a>
     </div>
